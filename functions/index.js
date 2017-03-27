57d052c3305b119d8e766091d0f3655b3400f26a
Wouter Vroege <woutervroege@MacBookProRetinaWV.fritz.box>
2017-03-27 14:50:09 +0200

    configie

--- functions/index.js
+++ functions/index.js
@@ -2,36 +2,24 @@ var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var nodemailer = require('nodemailer');
 var config = require('./config.json');
-var messageRef = functions.database.ref('/messages/{pushId}')
+var messageRef = functions.database.ref(config.database_ref_trigger)
 
 admin.initializeApp(functions.config().firebase);
 
-exports.sendEmail = functions.database.ref('/messages/{pushId}').onWrite(event => {
+exports.sendEmail = functions.database.ref(config.database_ref_trigger).onWrite(event => {
 
-    const original = event.data.val();
-
-    const transport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: config.email,
-            pass: config.password,
-        },
-    });
-
-    var styledMessage = original.message.replace("/\n/g", " \n ")
+    const userFormData = event.data.val();
+    const transport = nodemailer.createTransport(config.email_transport);
+    var styledMessage = userFormData.message.replace("/\n/g", " \n ");
 
     const mailOptions = {
-        from: 'Your lovely need-words bot',
-        to: 'samprinssen@gmail.com',
-        subject: 'Nieuw bericht op need-words',
-        html: `Hallo Marieke, <br>
-        er heeft iemand contact met je opgenomen op need-words.nl: <br>
-        ${original.name} (${original.email}) <br><br>
-
-        ${styledMessage} <br><br>
-
-        Ok doei! <br>
-        De bot`
+        from: config.email_from,
+        to: config.email_to,
+        subject: config.email_subject,
+        html: config.email_message_intro +
+        `${userFormData.name} (${userFormData.email}) <br><br>
+        ${styledMessage}` +
+        config.email_message_signature
     };
     transport.sendMail(mailOptions, (error, info) => {
         if (error) {
